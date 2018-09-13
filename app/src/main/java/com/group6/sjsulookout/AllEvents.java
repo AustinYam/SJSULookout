d7f29017dada3df04602f90368e899dd2a0d31dd
AustinYam <austin.yam@sjsu.edu>
2018-09-13 09:29:53 -0700

    Auth works not db yet

--- app/src/main/java/com/group6/sjsulookout/AllEvents.java
+++ app/src/main/java/com/group6/sjsulookout/AllEvents.java
@@ -1,19 +1,27 @@
 package com.group6.sjsulookout;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+
 public class AllEvents extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference myRef;
-    private  DatabaseReference mEventRef;
+    ListView myListView;
+    ArrayList<String> myArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +29,42 @@ public class AllEvents extends AppCompatActivity {
         setContentView(R.layout.activity_all_events);
 
         //Firebase
+        final ArrayList myArrayList = new ArrayList();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
-        myRef = mFirebaseDatabase.getReference();
-        //mEventRef = myRef.child()
-        mAuth = FirebaseAuth.getInstance();
+        myRef = mFirebaseDatabase.getReference().child("events");
+        myListView = (ListView) findViewById(R.id.ListEvent);
+        final ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myArrayList);
+        myListView.setAdapter(myArrayAdapter);
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    String myChildValues = dataSnapshot.child("title").getValue(String.class);
+                    myArrayList.add(myChildValues);
+                    myArrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
