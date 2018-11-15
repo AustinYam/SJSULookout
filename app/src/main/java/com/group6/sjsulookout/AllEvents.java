04c56a724f5bca4a0901ac8a68c52a193a14b475
AustinYam <austin.yam@sjsu.edu>
2018-11-15 15:03:47 -0800

    Adding SignUp and Events Page

--- app/src/main/java/com/group6/sjsulookout/AllEvents.java
+++ app/src/main/java/com/group6/sjsulookout/AllEvents.java
@@ -1,13 +1,17 @@
 package com.group6.sjsulookout;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -64,6 +68,15 @@ public class AllEvents extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
+            }
+        });
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(AllEvents.this, EventPage.class);
+                intent.putExtra("EventTitle", myArrayList.get(position));
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/group6/sjsulookout/EventPage.java
+++ app/src/main/java/com/group6/sjsulookout/EventPage.java
@@ -1,9 +1,14 @@
 package com.group6.sjsulookout;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -11,12 +16,20 @@ public class EventPage extends AppCompatActivity {
 
     private FirebaseDatabase mFirebaseDatabase;
     private TextView eventTitle, eventLocation, eventDate, eventDesc, eventContact;
-    private DatabaseReference mRef;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_page);
+
+        //Data from last activity
+        String eventTitle = getIntent().getStringExtra("EventTitle");
+
+        //Firebase
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference().child("events");
+
 
         TextView title = (TextView) findViewById(R.id.TitleView);
         TextView location = (TextView) findViewById(R.id.LocationView);
@@ -24,8 +37,34 @@ public class EventPage extends AppCompatActivity {
         TextView desc = (TextView) findViewById(R.id.DescView);
         TextView contact = (TextView) findViewById(R.id.ContactView);
 
+        title.setText(eventTitle);
 
+        myRef.orderByChild("title").equalTo(eventTitle).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
